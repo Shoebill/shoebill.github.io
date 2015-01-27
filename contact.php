22e41ea282c06069943d6121a0377cce8bc895a6
Marvin Haschker <catboy560@gmail.com>
2015-01-27 21:01:13 +0100

    test

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marvin
+ * Date: 10.01.15
+ * Time: 17:33
+ */
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Page for Shoebill SAMP Plugin">
+    <meta name="author" content="Marvin H">
+
+    <title>Shoebill</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">Shoebill</h3>
+                    <nav>
+                        <ul class="nav masthead-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="features.php">Features</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="inner cover">
+                <img class="cover-heading img-responsive" src="img/ShoebillLogoNew.png" style="margin-bottom: 5%">
+                <p class="lead">Shoebill provides the possibility to use your beautiful Java code with your Pawn-Gamemode. Or even write your whole gamemode in Java.</p>
+                <p>Use all your favorite Java libraries in samp. Make your server controllable via tcp, send emails directly from your server or make an inbuilt webserver? Everything is possible!</p>
+                <p class="lead">
+                    <a href="main.php" class="btn btn-lg btn-default">Learn more</a>
+                </p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
